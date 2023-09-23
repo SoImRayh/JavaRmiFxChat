7f83ea01784dcb8f3b22b54f987fb68091d0c8fc
rayh <raykah.silva@gmail.com>
2023-09-22 23:52:51 -0300

    tudo funcionando!

--- servidor/src/main/java/domain/Server.java
+++ servidor/src/main/java/domain/Server.java
@@ -1,17 +1,11 @@
 package domain;
 
-import domain.model.auth.Auth;
-import domain.model.auth.AuthDto;
-import domain.model.chat.Chat;
+
 import domain.model.chat.Message;
-import domain.model.util.Observer;
 import domain.model.util.RemoteObserver;
 
-import java.math.BigInteger;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.LinkedList;
-import java.util.List;
 
 public interface Server extends Remote {
 
